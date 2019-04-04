94d37d503e42d50109124bf97113933432c1e143
Max Friedrichsen <maxfred0025@gmail.com>
2019-04-04 14:54:22 -0700

    learning redux

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import SongList from './SongList';
+// named export so I use curly braces
+
+const App = () => {
+  return (
+    <div className="ui container grid">
+      <div className="ui row">
+        <div className="column eight wide">
+          <SongList />
+        </div>  
+      </div>
+    </div>
+  )
+}
+
+export default App;
